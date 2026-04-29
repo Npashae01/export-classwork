b74f3d31c1770c2f1f2f407128e504df68c01833
Adeyemi Babatope Silas <babatopesilasadeyemi@gmail.com>
2026-04-29 19:21:00 +0100

    Delete message.js

--- message.js
+++
@@ -1,6 +0,0 @@
-const { multiply, divide } = require('./calculator');
-const welcomeMessage = require('./message');
-
-console.log(welcomeMessage());
-console.log("Multiply:", multiply(4, 5));
-console.log("Divide:", divide(20, 4));
